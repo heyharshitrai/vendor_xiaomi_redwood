7612b1fc0ede387b72fa51a9ac264f9a94f631cc
New Author Name <johnivan19999@gmail.com>
2025-10-02 05:30:43 +0000

    redwood: audio: Use QCOM implementation for audio effects
    * AOSP effects doesn't give as thicc'n'sicc bass boost as the QC effects do.

--- redwood-vendor.mk
+++ redwood-vendor.mk
@@ -447,6 +447,9 @@ PRODUCT_PACKAGES += \
     libvpphvx \
     libvpplibrary \
     libxml \
+    libqcbassboost \
+    libqcreverb \
+    libqcvirt \
     vendor.qti.hardware.dsp@1.0 \
     vendor.qti.hardware.qconfig@1.0 \
     vendor.qti.hardware.vpp@1.1 \
